3911f994d8310389983c7d187a56cd6ea0e99be4
Vitalina Puzynia <102970899+Puzynia25@users.noreply.github.com>
2024-03-01 19:49:18 +0400

    feat: header layout done

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+    $(".header__slider").slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows-left" src="images/arrows-left.svg" alt=""/>',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="images/arrows-right.svg" alt=""/>',
+        asNavFor: ".slider-dots",
+    });
+    $(".slider-dots").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: ".header__slider",
+    });
+});
